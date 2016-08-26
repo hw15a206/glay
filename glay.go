602d47ca284265d0ba5314bc8d810adba399f859
ShaderKid <shaderkid.glsl@gmail.com>
2016-08-26 09:31:13 +0900

    add glay.go

---
+++ glay.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"runtime"
+)
+
+const defaultWindowWidth = 640
+const defaultWindowHeight = 480
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(defaultWindowWidth, defaultWindowHeight, "GLay", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	gl.ClearColor(0.0, 0.0, 0.0, 1.0)
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
